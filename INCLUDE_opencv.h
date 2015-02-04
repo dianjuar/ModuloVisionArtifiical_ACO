66c8ec2f0b70823791e2b2c311d8d06e7e5841c3
dianjuar <dianjuar@gmail.com>
2015-02-04 08:31:45 -0430

    prueba Staged

---
+++ INCLUDE_opencv.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_OPENCV_H
+#define INCLUDE_OPENCV_H
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+#endif // INCLUDE_OPENCV_H
--- ventanaprincipal.cpp
+++ ventanaprincipal.cpp
@@ -6,9 +6,51 @@ VentanaPrincipal::VentanaPrincipal(QWidget *parent) :
     ui(new Ui::VentanaPrincipal)
 {
     ui->setupUi(this);
+
+    cap = new STAND_capturadorImagen( STAND_capturadorImagen::Modo_Video, ui->Q_Ndispositivo_SpinBox->value() );
+
+    contectar_HiloCapturadorWITHVentanaPrincipal();
+
 }
+
+void VentanaPrincipal::set_labelDisplay(QImage QI)
+{
+    ui->label_display->setPixmap( QPixmap::fromImage( QI ));
+}
+
 
 VentanaPrincipal::~VentanaPrincipal()
 {
     delete ui;
+}
+
+void VentanaPrincipal::on_pushButton_clicked()
+{
+}
+
+void VentanaPrincipal::on_Q_Ndispositivo_SpinBox_valueChanged(int arg1)
+{
+    cap->~STAND_capturadorImagen();
+    set_labelDisplay( QImage("./media/CamNOT_FOUND.png") );
+
+    cap = new STAND_capturadorImagen(STAND_capturadorImagen::Modo_Video, arg1 );
+
+    contectar_HiloCapturadorWITHVentanaPrincipal();
+}
+
+void VentanaPrincipal::listen_matFromVideoCapture(QImage qi)
+{
+    set_labelDisplay(qi);
+}
+
+//el hilo lanzará la matriz proveniente de la camara y VentanaPrincipal la captará y la pondrá en la pantalla
+void VentanaPrincipal::contectar_HiloCapturadorWITHVentanaPrincipal()
+{
+    cap->start();
+
+    if( cap->isCamaraAbierta() )
+    {
+    connect(cap, SIGNAL(tell_Mat(QImage)),
+            this, SLOT(listen_matFromVideoCapture(QImage)));
+    }
 }
