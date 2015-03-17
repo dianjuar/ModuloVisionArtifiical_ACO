b4804098aa5188d49ec388715424fbb1b9dbf68d
dianjuar <dianjuar@gmail.com>
2015-03-16 22:50:26 -0430

    Optimización del proceso de calibracion

--- ventanaprincipal.h
+++ ventanaprincipal.h
@@ -11,6 +11,7 @@
 #include "config_partirncuadros.h"
 #include "config_INTMatBuilder.h"
 #include "config_matintsender.h"
+#include "config_calibrador.h"
 
 namespace Ui
 {
@@ -25,9 +26,22 @@ public:
     explicit VentanaPrincipal(QWidget *parent = 0);
     void set_labelDisplay( Mat );
     ~VentanaPrincipal();
+
+    static int FASE_NumeroFases;
+    static const int FASE_eleccionDeDispositivoDeGrabacion = 0;
+    static const int FASE_calibracion = 1;
+    static const int FASE_cortarContenido = 2;
+    static const int FASE_seleccionUmbral = 3;
+    static const int FASE_InicioFin = 4;
+    static const int FASE_PartinN = 5;
+    static const int FASE_EnvioEstacionCentral = 6;
     
 private slots:
+    void set_FotoTomada_calibracion(int);
+    void set_calibracionExitosa(bool);
+
     void listen_matFromVideoCapture();
+    void listen_matFromCalibrate();
 
     void on_Q_Ndispositivo_SpinBox_valueChanged(int arg1);
     void on_btn_siguiente_clicked();
@@ -52,6 +66,20 @@ private slots:
 
     void on_pushButton_2_clicked();
 
+    void on_pushButton_startF1_1_clicked();
+
+    void on_pushButton_restartF1_1_clicked();
+
+    void on_slider_BoardSizeW_F1_1_valueChanged(int value);
+
+    void on_slider_BoardSizeH_F1_1_valueChanged(int value);
+
+    void on_slider_NFotos_F1_1_valueChanged(int value);
+
+    void on_slider_delay_F1_1_valueChanged(int value);
+
+    void on_doubleSpinBox_F1_1_valueChanged(double arg1);
+
 private:
     Ui::VentanaPrincipal *ui;
 
@@ -63,13 +91,17 @@ private:
     CONFIG::partirNcuadros *PNcuadros;
     CONFIG::INTMatBuilder *IntMatB;
     CONFIG::matIntSender *mSender;
+    CONFIG::calibrador *calib;
 
     void contectar_HiloCapturadorWITHVentanaPrincipal();
+    void set_EnableSliderF1_1(bool enable, QPushButton *bMother, QPushButton *bOther);
+    void set_PorcenAvance_IN_progressBar(int NFaseCalib);
+    void pasarALaSiguienteEtapa();
 
     int config_index;
-    int config_Netapas;
+    int config_Nparametros;
 
-    bool F1_dispositivoValido;
+    int *numeroParametrosPorFaseCalib;
 };
 
 #endif // VENTANAPRINCIPAL_H
