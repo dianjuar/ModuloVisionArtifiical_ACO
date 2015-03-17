e89be5dd7701ffcfc08794f89e51ba1736b8e5ed
dianjuar <dianjuar@gmail.com>
2015-03-16 22:50:49 -0430

    Optimización del proceso de calibracion y creación de metodos para interactuar con la calibración

--- ventanaprincipal.cpp
+++ ventanaprincipal.cpp
@@ -1,6 +1,8 @@
 #include "ventanaprincipal.h"
 #include "ui_ventanaprincipal.h"
 
+int VentanaPrincipal::FASE_NumeroFases;
+
 VentanaPrincipal::VentanaPrincipal(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::VentanaPrincipal)
@@ -11,15 +13,15 @@ VentanaPrincipal::VentanaPrincipal(QWidget *parent) :
     QTextCodec::setCodecForCStrings(linuxCodec);
     QTextCodec::setCodecForLocale(linuxCodec);
 
-
     ui->setupUi(this);
 
-    //coloca todas las otras opciones desavilitadas para que el usuario no se salte los pasos
-    for(int i=1;i<ui->tabWidget->count();i++)
-        ui->tabWidget->setTabEnabled(i,false);
+    config_index = 0;
+    //ui->tabWidget->setCurrentIndex( config_index );
+    FASE_NumeroFases = ui->tabWidget->count();
 
-    cap = new STAND::capturadorImagen( STAND::capturadorImagen::Modo_Video, ui->Q_Ndispositivo_SpinBox->value() );
-    contectar_HiloCapturadorWITHVentanaPrincipal();
+    int modoElegido = STAND::capturadorImagen::Modo_Video;
+
+    cap = new STAND::capturadorImagen( modoElegido, ui->Q_Ndispositivo_SpinBox->value() );
 
     crop = new CONFIG::cropper( ui->slider_CannyU_1->value(), ui->slider_CannyU_2->value() );
     umb = new CONFIG::umbralizador( ui->slider_umbralBlackAndWhite->value() );
@@ -29,26 +31,68 @@ VentanaPrincipal::VentanaPrincipal(QWidget *parent) :
     PNcuadros = new CONFIG::partirNcuadros( ui->slider_n->value(), crop->get_tamano_MatrizCroped_SEGUIMIENTO() );
     IntMatB = new CONFIG::INTMatBuilder(umb->get_BlackAndWhite_SEGUIMIENTO(), PNcuadros->get_n(), crop->get_tamano_MatrizCroped_SEGUIMIENTO() );
     mSender = new CONFIG::matIntSender();
+    calib = new CONFIG::calibrador(ui->doubleSpinBox_F1_1->value(), Size(ui->slider_BoardSizeW_F1_1->value(), ui->slider_BoardSizeH_F1_1->value()),
+                                   ui->slider_NFotos_F1_1->value(), ui->slider_delay_F1_1->value());
 
     ui->label_error_F5->setText( mSender->MSJ_sinComprobar );
     ui->label_connectionTest_F5->setPixmap( QPixmap( mSender->RUTAIMG_incorrecto ) );
 
 
-    config_index =0;
-    config_Netapas = ui->tabWidget->count();
+
+    //variables utilizadas para manipular la barra de progreso del proceso de calibración
+    numeroParametrosPorFaseCalib = new int[FASE_NumeroFases];
+
+    numeroParametrosPorFaseCalib[ FASE_eleccionDeDispositivoDeGrabacion ] = 0;
+    numeroParametrosPorFaseCalib[ FASE_calibracion ] = 1;
+    numeroParametrosPorFaseCalib[ FASE_cortarContenido ] = 2;
+    numeroParametrosPorFaseCalib[ FASE_seleccionUmbral ] = 1;
+    numeroParametrosPorFaseCalib[ FASE_InicioFin ] = 5;
+    numeroParametrosPorFaseCalib[ FASE_PartinN ] = 1;
+    numeroParametrosPorFaseCalib[ FASE_EnvioEstacionCentral ] = 1;
+
+    config_Nparametros = 0;
+
+    for (int var = 0; var < FASE_NumeroFases; var++)
+        config_Nparametros += numeroParametrosPorFaseCalib[var];
+
+    //el hilo hará una llamada cada vez que capture una imagen por la cámara y la ventana principal atenderá esa llamada
+    connect(cap, SIGNAL(tell()),
+            this, SLOT(listen_matFromVideoCapture()));
+
+    connect(calib, SIGNAL(tell()),
+            this, SLOT(listen_matFromCalibrate()));
+
+    connect(calib, SIGNAL(fotoTomada(int)),
+            this, SLOT(set_FotoTomada_calibracion(int)));
+
+    connect(calib, SIGNAL(CalibracionExitosa(bool)),
+            this, SLOT(set_calibracionExitosa(bool)));
+
+
+    //coloca todas las otras opciones desavilitadas para que el usuario no se salte los pasos
+    for(int i=1;i<FASE_NumeroFases;i++)
+        ui->tabWidget->setTabEnabled(i,false);
 }
 
 void VentanaPrincipal::set_labelDisplay(Mat m)
 {
+
     switch(config_index)
     {
-        case 0:
+        case FASE_eleccionDeDispositivoDeGrabacion:
         {
             ui->label_displayF0->setPixmap( STAND::Tools::Mat2QPixmap(m) );
             break;
         }
 
-        case 1:
+        case FASE_calibracion:
+        {
+            ui->label_displayF1_1->setPixmap(STAND::Tools::Mat2QPixmap( m,2 ));
+
+            break;
+        }
+
+        case FASE_cortarContenido:
         {
             crop->calibracion( m );
             ui->label_displayF1->setPixmap( STAND::Tools::Mat2QPixmap( crop->get_ImagenRayada() , 2)  );
@@ -67,7 +111,7 @@ void VentanaPrincipal::set_labelDisplay(Mat m)
             break;
         }
 
-        case 2:
+        case FASE_seleccionUmbral:
         {
             Mat mCropped = m.clone();
             crop->cortarImagen(mCropped);
@@ -76,7 +120,7 @@ void VentanaPrincipal::set_labelDisplay(Mat m)
             break;
         }
 
-        case 3:
+        case FASE_InicioFin:
         {
             Mat mCropped = m.clone();
             crop->cortarImagen(mCropped);
@@ -97,7 +141,7 @@ void VentanaPrincipal::set_labelDisplay(Mat m)
             break;
         }
 
-        case 4:
+        case FASE_PartinN:
         {
             Mat mCropped = m.clone();
             crop->cortarImagen(mCropped);
@@ -118,72 +162,105 @@ VentanaPrincipal::~VentanaPrincipal()
     delete ui;
 }
 
+void VentanaPrincipal::set_FotoTomada_calibracion(int num)
+{
+    QString t = QString::number( num );
+    t.append("/");
+    t.append( QString::number(ui->slider_NFotos_F1_1->value()) );
+    ui->label_FotoTomada_F1_1->setText( t );
+}
+
+void VentanaPrincipal::set_calibracionExitosa(bool exito)
+{
+    if(exito)
+        ui->label_TodoEnOrden_F1_1->setPixmap( QPixmap::fromImage(QImage("./media/TestConnection/Right.png")) );
+    else
+        ui->label_TodoEnOrden_F1_1->setPixmap( QPixmap::fromImage(QImage("./media/TestConnection/error.png")) );
+}
+
 void VentanaPrincipal::on_Q_Ndispositivo_SpinBox_valueChanged(int arg1)
 {
-    if(cap->isCamaraAbierta())
-        disconnect(cap, SIGNAL(tell()),
-                   this, SLOT(listen_matFromVideoCapture()));
-
-    cap->~capturadorImagen();
+    cap->deviceChanged( arg1 );
     ui->label_displayF0->setPixmap( QPixmap::fromImage(QImage("./media/CamNOT_FOUND.png")) );
-
-    cap = new STAND::capturadorImagen(STAND::capturadorImagen::Modo_Video, arg1 );
-    contectar_HiloCapturadorWITHVentanaPrincipal();
-}
+ }
 
 void VentanaPrincipal::listen_matFromVideoCapture()
 {
     set_labelDisplay( cap->getImagen() );
 }
 
-//el hilo hará una llamada cada vez que capture una imagen por la cámara y la ventana principal atenderá esa llamada
-void VentanaPrincipal::contectar_HiloCapturadorWITHVentanaPrincipal()
+void VentanaPrincipal::listen_matFromCalibrate()
 {
-    F1_dispositivoValido = false;
-    cap->start();
+    set_labelDisplay( calib->get_m()  );
+}
 
-    if( cap->isCamaraAbierta() )
-    {
-        F1_dispositivoValido=true;
+void VentanaPrincipal::set_EnableSliderF1_1(bool enable, QPushButton *bMother,  QPushButton *bOther)
+{
+    ui->pushButton_startF1_1->setEnabled(enable);
+    ui->pushButton_restartF1_1->setEnabled(enable);
+    ui->doubleSpinBox_F1_1->setEnabled(enable);
 
-        connect(cap, SIGNAL(tell()),
-                this, SLOT(listen_matFromVideoCapture()));
-    }
-    else
-    {
-        qDebug()<<"no se pudo abrir el dispositivo";
-    }
+    ui->slider_BoardSizeH_F1_1->setEnabled(enable);
+    ui->slider_BoardSizeW_F1_1->setEnabled(enable);
+
+    ui->slider_NFotos_F1_1->setEnabled(enable);
+    ui->slider_delay_F1_1->setEnabled(enable);
+
+    bMother->setEnabled(false);
+    bOther->setEnabled(true);
+}
+
+void VentanaPrincipal::set_PorcenAvance_IN_progressBar(int NFaseCalib)
+{
+    int por_Progress=0;
+
+    for(int i;i<NFaseCalib;i++)
+        por_Progress += numeroParametrosPorFaseCalib[i];
+
+     por_Progress = por_Progress*100/FASE_NumeroFases;
+
+     ui->progressBar->setValue( por_Progress );
+}
+
+void VentanaPrincipal::pasarALaSiguienteEtapa()
+{
+    ui->tabWidget->setTabEnabled(++config_index,true);
+    ui->tabWidget->setCurrentIndex(config_index);
 }
 
 void VentanaPrincipal::on_btn_siguiente_clicked()
 {
     switch(config_index)
     {
-        case 0:
+        case FASE_eleccionDeDispositivoDeGrabacion:
         {
             if(cap->isCamaraAbierta())
             {
-                ui->tabWidget->setTabEnabled(++config_index,true);
                 ui->btn_atras->setEnabled(true);
-                ui->tabWidget->setCurrentIndex(config_index);
+                pasarALaSiguienteEtapa();
             }
             break;
         }
 
-        case 1:
-       {
-            if(crop->hayContenedor())
-            {
-                ui->tabWidget->setTabEnabled(++config_index,true);
-                ui->tabWidget->setCurrentIndex(config_index);
-            }
-            break;
-        }
-
-        case 2:
+        case FASE_calibracion:
         {
-            ui->tabWidget->setTabEnabled(++config_index,true);
-            ui->tabWidget->setCurrentIndex(config_index);
+            if(calib->get_todoEnOrden() || STAND::capturadorImagen::modo_elegido == STAND::capturadorImagen::Modo_ImagenStatica)
+                pasarALaSiguienteEtapa();
+
+            break;
+        }
+
+        case FASE_cortarContenido:
+       {
+            if(crop->hayContenedor())            
+               pasarALaSiguienteEtapa();
+
+            break;
+        }
+
+        case FASE_seleccionUmbral:
+        {
+            pasarALaSiguienteEtapa();
 
             qreal hipotenusa = qSqrt( 2*qPow( crop->get_tamano_MatrizCroped(),2 ) );
             ui->slider_HOUGH_param_2->setMaximum( hipotenusa );
@@ -194,12 +271,11 @@ void VentanaPrincipal::on_btn_siguiente_clicked()
             break;
         }
 
-        case 3:
+        case FASE_InicioFin:
         {
             if(cirD->get_calibracionCorrecta())
             {
-                ui->tabWidget->setTabEnabled(++config_index,true);
-                ui->tabWidget->setCurrentIndex(config_index);
+                pasarALaSiguienteEtapa();
 
                IntMatB->set_P_InicioYFin( cirD->get_PuntoInicio_SEGUIMIENTO(),cirD->get_PuntoFin_SEGUIMIENTO() );
                IntMatB->set_n( PNcuadros->get_n() );
@@ -208,17 +284,14 @@ void VentanaPrincipal::on_btn_siguiente_clicked()
         }
         break;
 
-        case 4:
+        case FASE_PartinN:
         {
-            if(!IntMatB->get_contieneError())
-            {
-                ui->tabWidget->setTabEnabled(++config_index,true);
-                ui->tabWidget->setCurrentIndex(config_index);
-            }
+            if(!IntMatB->get_contieneError())            
+                pasarALaSiguienteEtapa();
         }
         break;
 
-        case 5:
+        case FASE_EnvioEstacionCentral:
         {
             if( mSender->get_buenaConexion() )
             {
@@ -233,40 +306,32 @@ void VentanaPrincipal::on_tabWidget_currentChanged(int index)
 {
     config_index=index;
 
+    if(index!=FASE_calibracion && (!cap->isCamaraAbierta() || STAND::capturadorImagen::modo_elegido == STAND::capturadorImagen::Modo_ImagenStatica) )
+    {
+        calib->stop();
+        cap->InicicarHilo();
+    }
+
+    set_PorcenAvance_IN_progressBar( index );
+
+    //casos especiales
     switch(config_index)
     {
-        case 0:
-            ui->progressBar->setValue( 0 );
-        break;
-
-        case 1:
-            ui->progressBar->setValue( 1*100/11 );
-        break;
-
-        case 2:
-            ui->progressBar->setValue( 3*100/11 );
-        break;
-
-        case 3:
-            ui->progressBar->setValue( 4*100/11 );
-        break;
-
-        case 4:
-            ui->progressBar->setValue( 9*100/11 );
-        break;
-
-        case 5:
-            ui->progressBar->setValue( 10*100/11);
+        case FASE_calibracion:
+            cap->stop();
+            calib->InicicarHilo( cap->get_device() );
         break;
     }
+
+    qDebug()<<index;
 }
 
 void VentanaPrincipal::on_btn_atras_clicked()
 {
-    if(config_index!=0)
+    if(config_index != FASE_eleccionDeDispositivoDeGrabacion)
     {
         ui->tabWidget->setCurrentIndex(--config_index);
-        if(config_index ==0)
+        if(config_index == FASE_eleccionDeDispositivoDeGrabacion)
             ui->btn_atras->setEnabled(false);
     }
 }
@@ -351,4 +416,44 @@ void VentanaPrincipal::on_slider_CannyU_2_valueChanged(int value)
 void VentanaPrincipal::on_pushButton_2_clicked()
 {
     crop->reset_contenedor();
+}
+
+void VentanaPrincipal::on_pushButton_startF1_1_clicked()
+{
+    set_EnableSliderF1_1(false, ui->pushButton_startF1_1, ui->pushButton_restartF1_1);
+    calib->set_calibrar();
+}
+
+void VentanaPrincipal::on_pushButton_restartF1_1_clicked()
+{
+    set_EnableSliderF1_1(true, ui->pushButton_restartF1_1, ui->pushButton_startF1_1);
+    ui->label_TodoEnOrden_F1_1->setPixmap( QPixmap("./media/TestConnection/Bad.png") );
+    calib->set_standbay();
+}
+
+void VentanaPrincipal::on_slider_BoardSizeW_F1_1_valueChanged(int value)
+{
+    calib->set_boardSize( Size( ui->slider_BoardSizeW_F1_1->value(), ui->slider_BoardSizeH_F1_1->value()) );
+}
+
+void VentanaPrincipal::on_slider_BoardSizeH_F1_1_valueChanged(int value)
+{
+    calib->set_boardSize( Size( ui->slider_BoardSizeW_F1_1->value(), ui->slider_BoardSizeH_F1_1->value()) );
+}
+
+void VentanaPrincipal::on_slider_NFotos_F1_1_valueChanged(int value)
+{
+    set_FotoTomada_calibracion(0);
+
+    calib->set_NFotos( ui->slider_NFotos_F1_1->value() );
+}
+
+void VentanaPrincipal::on_slider_delay_F1_1_valueChanged(int value)
+{
+    calib->set_delay( ui->slider_delay_F1_1->value() );
+}
+
+void VentanaPrincipal::on_doubleSpinBox_F1_1_valueChanged(double arg1)
+{
+    calib->set_squareSize( ui->doubleSpinBox_F1_1->value() );
 }
