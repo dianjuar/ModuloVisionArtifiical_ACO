3245b8ad70ff47a66fb2427a44d5df78970984f4
dianjuar <dianjuar@gmail.com>
2015-03-20 10:55:08 -0430

    Quitando cosas inecesarias de la fase de detección inicio fin

--- ventanaprincipal.cpp
+++ ventanaprincipal.cpp
@@ -19,15 +19,15 @@ VentanaPrincipal::VentanaPrincipal(QWidget *parent) :
     //ui->tabWidget->setCurrentIndex( config_index );
     FASE_NumeroFases = ui->tabWidget->count();
 
-    int modoElegido = STAND::capturadorImagen::Modo_Video;
+    int modoElegido = STAND::capturadorImagen::Modo_ImagenStatica;
 
     cap = new STAND::capturadorImagen( modoElegido, ui->Q_Ndispositivo_SpinBox->value() );
 
     crop = new CONFIG::cropper( ui->slider_CannyU_1->value(), ui->slider_CannyU_2->value() );
     umb = new CONFIG::umbralizador( ui->slider_umbralBlackAndWhite->value() );
-    cirD = new CONFIG::circleDetect( ui->slider_HOUGH_min_dist->value(),
+    /*cirD = new CONFIG::circleDetect( ui->slider_HOUGH_min_dist->value(),
                                      ui->slider_HOUGH_param_1->value(), ui->slider_HOUGH_param_2->value(),
-                                     ui->slider_HOUGH_min_radius->value(), ui->slider_HOUGH_max_radius->value());
+                                     ui->slider_HOUGH_min_radius->value(), ui->slider_HOUGH_max_radius->value());*/
     PNcuadros = new CONFIG::partirNcuadros( ui->slider_n->value(), crop->get_tamano_MatrizCroped_SEGUIMIENTO() );
     IntMatB = new CONFIG::INTMatBuilder(umb->get_BlackAndWhite_SEGUIMIENTO(), PNcuadros->get_n(), crop->get_tamano_MatrizCroped_SEGUIMIENTO() );
     mSender = new CONFIG::matIntSender(ui->lineEdit_setverDir_F5->text());
@@ -67,7 +67,6 @@ VentanaPrincipal::VentanaPrincipal(QWidget *parent) :
 
     connect(calib, SIGNAL(CalibracionExitosa(bool)),
             this, SLOT(set_calibracionExitosa(bool)));
-
 
     //coloca todas las otras opciones desavilitadas para que el usuario no se salte los pasos
     for(int i=1;i<FASE_NumeroFases;i++)
@@ -122,22 +121,7 @@ void VentanaPrincipal::set_labelDisplay(Mat m)
 
         case FASE_InicioFin:
         {
-            Mat mCropped = m.clone();
-            crop->cortarImagen(mCropped);
-            cirD->calibrar(mCropped);
-
-            ui->label_displayF3->setPixmap( STAND::Tools::Mat2QPixmap( mCropped,true ) );
-
-            if( cirD->get_calibracionCorrecta() )
-            {
-                ui->label_errorText_F3->setText("Todo en Orden");
-                ui->label_errorImg_F3->setPixmap( QPixmap("./media/TestConnection/Right.png")  );
-            }
-            else
-            {
-                ui->label_errorText_F3->setText("No se ha detectado los circulos necesarios");
-                ui->label_errorImg_F3->setPixmap( QPixmap("./media/TestConnection/Bad.png")  );
-            }
+            ui->label_display_IF->setPixmap(  STAND::Tools::Mat2QPixmap( IntMatB->get_MatCartooned() ) );
             break;
         }
 
@@ -148,9 +132,9 @@ void VentanaPrincipal::set_labelDisplay(Mat m)
             PNcuadros->calibrar(mCropped);
 
             ui->label_displayF4->setPixmap( STAND::Tools::Mat2QPixmap( mCropped,true,
-                                                                      IntMatB->get_tamano_MatCartooned()) );
+                                                                     IntMatB->get_tamano_MatCartooned()) );
             ui->label_displayF4_Cartoon->setPixmap(  STAND::Tools::Mat2QPixmap( IntMatB->get_MatCartooned(),true,
-                                                                               IntMatB->get_tamano_MatCartooned())  );
+                                                                                IntMatB->get_tamano_MatCartooned())  );
             break;
         }
     }
@@ -262,12 +246,7 @@ void VentanaPrincipal::on_btn_siguiente_clicked()
         {
             pasarALaSiguienteEtapa();
 
-            qreal hipotenusa = qSqrt( 2*qPow( crop->get_tamano_MatrizCroped(),2 ) );
-            ui->slider_HOUGH_param_2->setMaximum( hipotenusa );
-            ui->slider_HOUGH_param_1->setMaximum( hipotenusa );
-            ui->slider_HOUGH_min_radius->setMaximum( hipotenusa );
-            ui->slider_HOUGH_min_dist->setMaximum( hipotenusa );
-            ui->slider_HOUGH_max_radius->setMaximum( hipotenusa );
+            IntMatB->set_n( PNcuadros->get_n() );
             break;
         }
 
@@ -276,21 +255,19 @@ void VentanaPrincipal::on_btn_siguiente_clicked()
             if(cirD->get_calibracionCorrecta())
             {
                 pasarALaSiguienteEtapa();
-
-               IntMatB->set_P_InicioYFin( cirD->get_PuntoInicio_SEGUIMIENTO(),cirD->get_PuntoFin_SEGUIMIENTO() );
-               IntMatB->set_n( PNcuadros->get_n() );
             }
             break;
         }
         break;
 
         case FASE_PartinN:
-        {
-            if(!IntMatB->get_contieneError())
-            {
-                pasarALaSiguienteEtapa();
+        {            
+            pasarALaSiguienteEtapa();
+
+            if(STAND::capturadorImagen::modo_elegido == STAND::capturadorImagen::Modo_Video)
                 calib->set_distanciaEntreCuadros( PNcuadros->get_distanciaEnPXentreCuadros(), PNcuadros->get_n() );
-            }
+            else
+                calib->set_distanciaEntreCuadros(1.0);
         }
         break;
 
@@ -340,31 +317,6 @@ void VentanaPrincipal::on_btn_atras_clicked()
 void VentanaPrincipal::on_slider_umbralBlackAndWhite_valueChanged(int value)
 {
     umb->setUmbral( value );
-}
-
-void VentanaPrincipal::on_slider_HOUGH_min_dist_valueChanged(int value)
-{
-    cirD->set_HOUGH_min_dist( value );
-}
-
-void VentanaPrincipal::on_slider_HOUGH_param_1_valueChanged(int value)
-{
-    cirD->set_HOUGH_param_1( value );
-}
-
-void VentanaPrincipal::on_slider_HOUGH_param_2_valueChanged(int value)
-{
-    cirD->set_HOUGH_param_2( value );
-}
-
-void VentanaPrincipal::on_slider_HOUGH_min_radius_valueChanged(int value)
-{
-    cirD->set_HOUGH_min_radius( value );
-}
-
-void VentanaPrincipal::on_slider_HOUGH_max_radius_valueChanged(int value)
-{
-    cirD->set_HOUGH_max_radius( value );
 }
 
 void VentanaPrincipal::on_slider_n_valueChanged(int value)
@@ -455,4 +407,16 @@ void VentanaPrincipal::on_pushButton_ProbarConexion_EstCentral_clicked()
         ui->label_error_F5->setText( mSender->MSJ_incorrecto );
         ui->label_connectionTest_F5->setPixmap( QPixmap( mSender->RUTAIMG_incorrecto ) );
     }
+}
+
+void VentanaPrincipal::on_pushButton_setPI_IF_clicked()
+{
+    ui->label_I_IF->setEnabled(true);
+    ui->label_F_IF->setEnabled(false);
+}
+
+void VentanaPrincipal::on_pushButton_setPF_IF_clicked()
+{
+    ui->label_I_IF->setEnabled(false);
+    ui->label_F_IF->setEnabled(true);
 }
